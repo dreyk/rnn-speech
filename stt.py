910d54de0bee1540e89cceca667f3b3cec61036d
Alexander Gunin <guninalexander@gmail.com>
2018-01-21 23:50:44 +0300

    distributed training

--- stt.py
+++ stt.py
@@ -131,6 +131,7 @@ def build_acoustic_training_rnn(is_chief,is_ditributed,sess, hyper_params, prog_
             is_chief=is_chief,
             logdir=prog_params["train_dir"],
             init_op=init_op,
+            local_init_op = tf.control_flow_ops.group(t_iterator.initializer,v_iterator.initializer),
             recovery_wait_secs=1,
             summary_op=None,
             global_step=model.global_step)
@@ -279,15 +280,6 @@ def distributed_train_acoustic_rnn(train_set, test_set, hyper_params, prog_param
         is_chief = prog_params["is_chief"]
         sv, model, t_iterator, v_iterator = build_acoustic_training_rnn(is_chief, True,sess, hyper_params,
                                                                     prog_params, train_set, test_set)
-        if t_iterator is None:
-            logging.Info("Skip init t iterator")
-        else:
-            sess.run(t_iterator.initializer)
-
-        if v_iterator is None:
-            logging.Info("Skip init v iterator")
-        else:
-            sess.run(v_iterator.initializer)
         with sv.managed_session(server.target, config=sess_config) as sess:
             previous_mean_error_rates = []
             current_step = epoch = 0
